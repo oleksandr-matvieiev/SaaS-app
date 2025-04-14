13d28a49bd02af3bc1c3232768be21d408dde70c
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-04-14 21:31:49 +0200

    init project

--- src/main/java/com/petproject/saasapp/SaaSAppApplication.java
+++ src/main/java/com/petproject/saasapp/SaaSAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SaaSAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SaaSAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SaaSAppApplication.class, args);
+    }
 
 }
