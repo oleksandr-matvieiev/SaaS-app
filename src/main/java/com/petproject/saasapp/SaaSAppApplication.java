750dacbcd60f2e19c375cb0708945334ae897c43
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-04-14 19:30:57 +0200

    first commit

---
+++ src/main/java/com/petproject/saasapp/SaaSAppApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.saasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaSAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaaSAppApplication.class, args);
+	}
+
+}
