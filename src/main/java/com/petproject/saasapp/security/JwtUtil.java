d2717a57903487e3dcf389e3f9efa907e4f88331
oleksandr-matvieiev <oleksandr.matvieiev.d@gmail.com>
2025-04-18 14:26:05 +0200

    Added roles to JWT token

--- src/main/java/com/petproject/saasapp/security/JwtUtil.java
+++ src/main/java/com/petproject/saasapp/security/JwtUtil.java
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtUtil {
@@ -20,6 +21,7 @@ public class JwtUtil {
 
     @Value("${jwt.expiration}")
     private Long expiration;
+
     private SecretKey key;
 
     @PostConstruct
@@ -27,9 +29,13 @@ public class JwtUtil {
         this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     }
 
-    public String generateToken(String email) {
+    public String generateToken(String email, List<String> roles) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", roles);
+
         return Jwts.builder()
                 .subject(email)
+                .claims(claims)
                 .issuedAt(new Date())
                 .expiration(new Date((new Date()).getTime() + expiration))
                 .signWith(key)
@@ -46,17 +52,35 @@ public class JwtUtil {
         return claims.getSubject();
     }
 
+    public List<String> getRolesFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+        Object rolesObject = claims.get("roles");
+
+        if (rolesObject instanceof List<?>) {
+            return ((List<?>) rolesObject).stream()
+                    .map(Object::toString)
+                    .collect(Collectors.toList());
+        }
+
+        return Collections.emptyList();
+    }
+
     public boolean validateToken(String token) {
         try {
             Jwts.parser().verifyWith(key).build().parse(token);
             return true;
         } catch (SecurityException | MalformedJwtException e){
             throw new AuthenticationCredentialsNotFoundException("JWT was expired or incorrect.");
-        }catch (ExpiredJwtException e){
+        } catch (ExpiredJwtException e){
             throw new AuthenticationCredentialsNotFoundException("Expired JWT token.");
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e){
             throw new AuthenticationCredentialsNotFoundException("JWT is invalid.");
-        }catch (UnsupportedJwtException e){
+        } catch (UnsupportedJwtException e){
             throw new AuthenticationCredentialsNotFoundException("Unsupported JWT token.");
         }
     }
